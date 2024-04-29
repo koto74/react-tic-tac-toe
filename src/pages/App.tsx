45829e9f56ae41a228ae407f4c42df748a9035a8
koto74 <kotoganaiyo@gmail.com>
2024-04-29 11:43:10 +0900

    feat decision of victory

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,7 +5,7 @@ type SquareProps = {
   onSquareClick: () => void
 }
 
-export const Board:React.FC = () => {
+export const Board: React.FC = () => {
   const [xIsNext, setXIsNext] = useState<boolean>(true)
   const [squares, setSquares] = useState<Array<'X' | 'O' | null>>(Array(9).fill(null))
 
@@ -16,16 +16,25 @@ export const Board:React.FC = () => {
     const newSquares = squares.slice()
     if (xIsNext) {
       newSquares[i] = 'X'
-    }
-    else {
+    } else {
       newSquares[i] = 'O'
     }
     setSquares(newSquares)
     setXIsNext(!xIsNext)
   }
+  
+  const winner = caluculateWinner(squares)
+  let status
+  if (winner) {
+    status = `Winner: ${winner}`
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`
+  }
+
 
   return (
     <>
+      <div className='status'>{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -45,6 +54,30 @@ export const Board:React.FC = () => {
   )
 }
 
-const Square:React.FC<SquareProps> = ({ value, onSquareClick }) => {
-  return <button className="square" onClick={onSquareClick}>{value}</button>
+const Square: React.FC<SquareProps> = ({ value, onSquareClick }) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  )
+}
+
+const caluculateWinner = (squares: Array<'X' | 'O' | null>): 'X' | 'O' | null => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
 }
