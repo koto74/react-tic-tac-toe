b3a94ec1de80c75f6e5d1b53431b49169ddb45c3
koto74 <kotoganaiyo@gmail.com>
2024-04-29 16:20:05 +0900

    feat: jump to

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -60,15 +60,36 @@ const Board: React.FC<BoardProps> = ({ xIsNext, squares, onPlay }): JSX.Element 
 }
 
 export const Game: React.FC = (): JSX.Element => {
-  const [xIsNext, setXIsNext] = useState<boolean>(true)
   const [squares, setSquares] = useState<Array<'X' | 'O' | null>>(Array(9).fill(null))
   const [history, setHistory] = useState<Array<Array<'X' | 'O' | null>>>([squares])
-  const currentSquares = history[history.length - 1]
+  const [currentMove, setCurrentMove] = useState<number>(0)
+  const xIsNext = currentMove % 2 === 0
+  const currentSquares = history[currentMove]
 
   const handlePlay = (nextSquares: Array<'X' | 'O' | null>): void => {
-    setHistory([...history, nextSquares])
-    setXIsNext(!xIsNext)
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1)
   }
+  
+  const jumpTo = (nextMove: number): void => {
+    setCurrentMove(nextMove)
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to move #${move}`
+    } else {
+      description = 'Go to game start'
+    }
+    
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
 
   return (
     <div className="game">
@@ -76,7 +97,7 @@ export const Game: React.FC = (): JSX.Element => {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
